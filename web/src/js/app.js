894e08428d8a4436a84927e3c90a6d3408513960
Ian Petrarca <ianpetrarca@users.noreply.github.com>
2021-05-19 15:33:01 -0400

    Update app.js

--- web/src/js/app.js
+++ web/src/js/app.js
@@ -11,13 +11,13 @@ document.getElementById('get_ipfs_btn').addEventListener("click", validateLink);
 
 //Get Meta Data using BCD API with an inputted Objkt ID
 function getMetaData(id) {
-
+    //Clear Fields
     document.getElementById('ipfs_output').innerHTML = ''
     document.getElementById('id_output').innerHTML = ''
     document.getElementById('creator_output').innerHTML = ''
     document.getElementById('thumb_output').innerHTML = ''
     document.getElementById('description_output').innerHTML = ''
-    
+    //Get Metadata then insert API response
     getTokenInfo(id).then((response) => {  
         document.getElementById('ipfs_output').innerHTML = '<span class="text-blue-800">IPFS Link: </span>' + 'https://cloudflare-ipfs.com/ipfs/' + response.artifact_uri.slice(7,response.artifact_uri.length)
         document.getElementById('id_output').innerHTML = '<span class="text-blue-800">OBJKT ID: </span>' + response.token_id
